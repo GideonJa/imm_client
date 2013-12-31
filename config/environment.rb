aedb3f3cab00bc286f21ae7b1f4cef9400420bbd
Gideon jadlovker <gideonj1@gmail.com>
2013-12-31 14:46:54 -0500

    empty db

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Immune::Application.initialize!
-
-config.frameworks -= [ :active_record, :active_resource, :action_mailer]
